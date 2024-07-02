8520b2a10967cd69d30b4b22ee43b558b8f1d17f
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2024-07-02 11:36:58 -0300

    save progess

---
+++ README.md
@@ -0,0 +1,13 @@
+```shel```
+```shel```
+
+
+1. Download the model
+```shel
+huggingface-cli download microsoft/Phi-3-mini-128k-instruct-onnx --include cpu_and_mobile/cpu-int4-rtn-block-32-acc-level-4/* --local-dir .
+```
+
+
+```shel
+pip install -r "requirements.txt"
+```
---
+++ search-dir.py
@@ -0,0 +1,27 @@
+import onnxruntime_genai as og
+
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import SpacyEmbeddings
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+loader = PyPDFDirectoryLoader("data/")
+docs = loader.load()
+
+embeddings = SpacyEmbeddings(model_name="pt_core_news_lg")
+db = FAISS.from_documents(docs, embeddings)
+
+
+def retrive_info(query):
+    similar_response = db.similarity_search(query, k=3)
+    # print(*similar_response, sep='\n')
+    return [doc.page_content for doc in similar_response]
+
+
+text = input("Input: ")
+if not text:
+    print("Error, input cannot be empty")
+    exit
+
+context = retrive_info(text)
+
+print(context)
